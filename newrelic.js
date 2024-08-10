0d0f068cdc70e58eb7b18c36c65389ed91a1c60c
Lobo045 <lopezabraham@outlook.com>
2024-08-10 14:28:33 -0600

    git flow

---
+++ newrelic.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * New Relic agent configuration.
+ */
+exports.config = {
+  app_name: ['Nombre de Tu Aplicación'],
+  license_key: 'NRAK-UDKYIYDLC1OF3NH8MZA8MJWZA5I',
+  logging: {
+    level: 'info',
+  },
+};
